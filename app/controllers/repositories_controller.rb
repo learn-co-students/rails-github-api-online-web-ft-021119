0e892ded7c65600cbbffffe8477b9ce42d91d35e
AudreyJones <audrey.eliz.jones@gmail.com>
2019-06-07 14:54:24 -0400

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,7 +6,6 @@ class RepositoriesController < ApplicationController
    end
    body = JSON.parse(response.body)
    @user = body["login"]
-   # repo_response = Faraday.get "#{body["repos_url"]}"
    repo_response = Faraday.get "https://api.github.com/user/repos" do |req|
       req.headers = {"authorization": "token #{session[:token]}"}
    end
